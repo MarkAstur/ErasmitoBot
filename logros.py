86bab6f8f5e36d14fe92d82c1798728cb1800b60
MarkAstur <markastur@outlook.com>
2025-05-16 15:49:12 +0200

    Update logros.py

--- logros.py
+++ logros.py
@@ -6,16 +6,22 @@ def revisar_logros(user_id):
     nuevos = []
 
     if mensajes >= 1:
-        nuevos.append("💬 Mentat desmemoriado")
+        nuevos.append("🧠 Mentat desmemoriad@")
     if mensajes >= 100:
         nuevos.append("💬 Muad'Dib del Chat")
     if mensajes >= 1000:
         nuevos.append("🧠 Mentat del Foro")
+    if reacciones >= 1:
+        nuevos.append("💛 Novat@ en la Prescencia")
     if reacciones >= 20:
         nuevos.append("🎭 Fremen Popular")
     if tiempo_ingreso:
         dias = (datetime.now() - datetime.fromisoformat(tiempo_ingreso)).days
         if dias >= 30:
             nuevos.append("📅 Fremen del Sietch")
+    if tiempo_ingreso:
+        dias = (datetime.now() - datetime.fromisoformat(tiempo_ingreso)).days
+        if dias >= 1:
+            nuevos.append("📅 Trucha de Arena Recién Nacida")
 
     return guardar_logros(user_id, nuevos)
