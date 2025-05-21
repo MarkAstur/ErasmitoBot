ffbdfc0b87fa54ac9986a7ea2f4ae911870e39d5
MarkAstur <markastur@outlook.com>
2025-05-21 09:50:48 +0000

    Update logros.py

--- logros.py
+++ logros.py
@@ -16,7 +16,7 @@ LOGROS = [
     #LOGROS MENCIONES
     {"nombre": "Novato de la Voz - Mencionas a un Usuario", "condicion": lambda d: d.get("menciones", 0) >= 1, "imagen": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/1689500/05f9c48a93e12754a31d03a1f1f52a4cbc254eb1.jpg"},
     #LOGROS ENTRAR CANALES
-    {"nombre": "Uso de la Voz - Primera vez que entras en un canal de Voz", "condicion": lambda d: d.get("voz", 0) >= 1, "imagen": "https://static.wikia.nocookie.net/dune/images/8/8c/51oT6J972oL._SY445_-1.jpg"},
+    {"nombre": "Uso de la Voz - Primera vez que entras en un canal de Voz", "condicion": lambda d: d.get("voz", 0) >= 1, "imagen": "https://i.ibb.co/zHV0wLTq/00.png"},
 
 ]
 
